176ff433ef822a5978f02eb837f18e885e022a1f
AryanJ03 <63674350+AryanJ03@users.noreply.github.com>
2024-02-22 16:27:40 +0000

    Update main.py
    added User table model

--- project/main.py
+++ project/main.py
@@ -1,6 +1,16 @@
 from flask import Blueprint, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 main = Blueprint('main', __name__)
+db = SQLAlchemy(main)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(25), nullable=False)
+    type = db.Column(db.String(2), nullable=False)
 
 @main.route('/')
 def index():
